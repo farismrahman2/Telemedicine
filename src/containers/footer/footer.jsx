7b94dc3afac0d1cf47385ec5da76e3011425a411
Zach <zstover@students.kennesaw.edu>
2022-02-08 13:07:48 -0500

    changes to footer 2-zstover

--- src/containers/footer/footer.jsx
+++ src/containers/footer/footer.jsx
@@ -5,17 +5,13 @@ import logo from '../../assets/logo.png';
 const Footer = () => {
   return (
   <div className = "footer section__padding">
-    <div className = "footer-heading">
-      <h1 className = "gradient__text">[Fill in the blank]</h1>
-    </div>
     <div className = "footer-btn">
       <p><a href='#home'>Return to Top of Page</a></p>
-
     </div>
     <div className ="footer-links">
       <div className ="footer-links_logo">
         <img src={logo} alt="logo" ></img>
-        <p>Adress goes here!</p>
+        <p>1100 South Marietta Pkwy SE</p>
       </div>
       <div className ="footer-links_div">
         <h4>Links</h4>
@@ -24,9 +20,9 @@ const Footer = () => {
       </div>
       <div className ="footer-links_div">
         <h4>Get in touch</h4>
-        <p>Adress?</p>
-        <p>Phone Number?</p>   
-        <p>Email?</p> 
+        <p>1100 South Marietta Pkwy </p>
+        <p>470-353-7879</p>   
+        <p>telemedicine42@gmail.com</p> 
       </div> 
     </div>
     <div className = "footer-copyright">
