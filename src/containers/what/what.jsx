7930666648e2c521a9204ab0b177a2d9563329ca
Adam Whyte <awhyte@students.kennesaw.edu>
2022-02-16 00:32:08 -0500

    What.jsx and what.css

--- src/containers/what/what.jsx
+++ src/containers/what/what.jsx
@@ -2,7 +2,101 @@ import React from 'react';
 import './what.css';
 
 const What = () => {
-  return <div>What (What we Treat) //Adam </div>;
+    return <div>
+        <div class="Outer">
+            <h5 class="headline">Cancer Care</h5>
+            <div class="ParagraphContent">
+                <h6 class="headline-5"></h6>
+                <p class="body-1">
+                </p><p>Every person with cancer is unique, from your genetic makeup to your body, from your beliefs and lifestyle to the people in your life. You are not the same as anyone else, and neither is your treatment plan at Wellstar Health System.</p>
+                <p></p>
+            </div>
+            <div class="ParagraphContent">
+                <h6 class="headline-5">Brain &amp; Spine Tumor STAT Clinic</h6>
+                <p class="body-1">
+                    A diagnosis of a brain or spine tumor affects each person differently. While for some people, the tumor's location in the central nervous system can cause disorientation, an altered mental state, dizziness and seizures, for others the symptoms will look different.
+                </p>
+            </div>
+            <div class="ParagraphContent">
+                <h6 class="headline-5">Breast Cancer STAT Clinic</h6>
+                <p class="body-1">
+                    Get the best possible care for breast cancer here, STAT.
+                </p>
+            </div>
+            <div class="ParagraphContent">
+                <h6 class="headline-5">Lung Cancer STAT Clinic</h6>
+                <p class="body-1">
+                    Our Specialty Teams and Treatments (STAT) Clinic helps patients with lung and esophageal cancer get answers and start treatment faster, leading to better outcomes.
+                </p>
+            </div>
+            <div class="ParagraphContent">
+                <h6 class="headline-5">Nurse Navigation</h6>
+                <p class="body-1">
+                    Many people treated for cancer at Wellstar could not imagine care without their nurse navigator. Patients say nurse navigators are a reason they can face cancer with confidence.
+                </p>
+            </div>
+                <div class="ParagraphContent">
+                    <h6 class="headline-5">Pancreatic Hepatobiliary Malignancies Program</h6>
+                    <p class="body-1">
+                        For patients with cancer of the pancreas, bile duct system and liver, metro Atlanta’s gastrointestinal cancer experts offer a higher level of care.
+                </p>
+                </div>
+                <div class="ParagraphContent">
+                    <h6 class="headline-5">Prostate Cancer STAT Clinic</h6>
+                    <p class="body-1">
+                        Our Specialty Teams and Treatments (STAT) Clinic for Prostate Cancer gives men and their partners a high level of comprehensive care, including a multidisciplinary team of leading specialists and access to state-of-the-art treatments.
+                </p>
+                </div>
+                <div class="ParagraphContent">
+                    <h6 class="headline-5">Rectal Cancer Multidisciplinary Program </h6>
+                    <p class="body-1">
+                        The highest level of care for people with rectal cancer—fast and close to home.
+                </p>
+                </div>
+                <div class="ParagraphContent">
+                    <h6 class="headline-5">Gynecologic Oncology</h6>
+                    <p class="body-1">
+                    </p><p>Wellstar Gynecology Oncology specializes in state-of-the-art, individualized care for cancers of the female reproductive system.</p>
+                    <p></p>
+                </div>
+                <div class="ParagraphContent">
+                    <h6 class="headline-5">Hematology Oncology</h6>
+                    <p class="body-1">
+                        Hematology-Oncology specializes in diagnosing, treating and preventing a broad range blood cancers. Our experts, trained in hematology (the study of blood) and oncology (the study of cancer), develop personalized, comprehensive treatment plans for diseases such as Hodgkin's and non-Hodgkin's lymphomas, leukemias and multiple myelomas.
+                </p>
+                </div>
+                <div class="ParagraphContent">
+                    <h6 class="headline-5">Medical Oncology</h6>
+                    <p class="body-1">
+                        Our medical oncologists give patients access to the latest in genetically based targeted therapies, immunotherapy and chemotherapy to fight various types of cancer. Patients who qualify have access to new or experimental treatments before they are commercially available through clinical trials at select locations.&nbsp;
+                </p>
+                </div>
+                <div class="ParagraphContent">
+                    <h6 class="headline-5">Nuclear Medicine</h6>
+                    <p class="body-1">
+                        Nuclear medicine is advanced imaging that allows physicians to see detailed snapshots of a patient’s tissues and organs. This type of imaging helps physicians make earlier diagnoses and give patients more effective treatment.
+                </p>
+                </div>
+                <div class="ParagraphContent">
+                    <h6 class="headline-5">Radiation Oncology</h6>
+                    <p class="body-1">
+                        Radiation Oncology combines state-of-the-art equipment and advanced technology with compassion to provide the highest level of care for patients with cancer and some benign conditions.
+                </p>
+                </div>
+                <div class="ParagraphContent">
+                    <h6 class="headline-5">Surgical Oncology</h6>
+                    <p class="body-1">
+                        Surgical oncology is part of Wellstar’s compassionate, patient-centered cancer care team. Our renowned surgical oncologists diagnose and treat cancer using surgical techniques. Surgery is often combined with other treatments like chemotherapy or radiation to give you the best chance for a cancer-free future.
+                </p>
+                </div>
+                <div class="ParagraphContent">
+                    <h6 class="headline-5">Therapeutic Radiology</h6>
+                    <p class="body-1">
+                        Radiation Oncology uses radiation to destroy or shrink tumors.
+                </p>
+            </div>
+                </div>
+    </div>
 };
 
 export default What;
